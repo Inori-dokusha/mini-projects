99a3cf07c12d193c512ed2d22e93746a4a96833b
Inori-dokusha <inori.dokusha@gmail.com>
2025-09-08 16:58:44 +0700

    add copy feature

--- color-flipper/index.js
+++ color-flipper/index.js
@@ -1,16 +1,30 @@
-document.querySelector("button").addEventListener("click", () => {
-    const result = random(["r1", "g1", "b1", "r2", "g2", "b2"]);
-    const convert = `#${result.join('')}`;
-    document.querySelector("#color").innerHTML = convert;
-    document.body.style.backgroundColor = convert;
+let value = document.querySelector("#color");
+value.textContent = "#FFFFFF";
+document.body.style.backgroundColor = "#FFFFFF";
+
+document.querySelector(".random").addEventListener("click", () => {
+    const color = generateRandomColor();
+    value.textContent = color;
+    document.body.style.backgroundColor = color
 });
 
-const random = (colors) => {
+document.querySelector(".copy").addEventListener("click", () => {
+    const text = document.querySelector("#color").textContent;
+    navigator.clipboard.writeText(text);
+    const alertElement = document.querySelector(".alert");
+    alertElement.classList.add('on');
+    setTimeout(() => {
+        alertElement.classList.remove("on")
+    }, 1000);
+});
+
+function generateRandomColor() {
     const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
-    const result = [];
-    for (let color of colors) {
-        color = Math.trunc(Math.random() * 16);
-        result.push(hex[color])
+    let colorString = "#";
+    for (var i = 0; i < 6; i++) {
+        const getNum = Math.trunc(Math.random() * 16);
+        const result = hex[getNum];
+        colorString += result;
     }
-    return result;
+    return colorString;
 }
