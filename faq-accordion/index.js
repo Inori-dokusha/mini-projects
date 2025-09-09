25c9817cd722b23379eb788a3682c02aa76f85e0
inori.dokusha <inori.dokusha@gmail.com>
2025-09-09 04:25:08 +0000

    save file

--- faq-accordion/index.js
+++ faq-accordion/index.js
@@ -2,6 +2,6 @@ document.querySelectorAll(".quest").forEach((el) => {
     el.setAttribute("data-active", false);
     el.addEventListener("click", () => {
         const active = el.getAttribute("data-active");
-        console.log(active);
+        console.log(active.data.set = );
     });
 });
