fc9bcdeae2039910619b275c57d8da7b8f2abc61
inori.dokusha <inori.dokusha@gmail.com>
2025-09-26 03:25:11 +0000

    finish

--- todo-list/index.js
+++ todo-list/index.js
@@ -1,23 +1,9 @@
-const todo = [
-    {
-        text: "Learn HTML",
-        done: false,
-    },
-    {
-        text: "Learn CSS",
-        done: false,
-    },
-    {
-        text: "Learn JS",
-        done: false,
-    },
-];
+import todo from "./todo.js";
 
 //Get all elements
 const input = document.querySelector("#todoText");
 const button = document.querySelector("#addTodo");
 const ul = document.querySelector("#todoList");
-const filtersButton = document.querySelectorAll(".filters button");
 
 //Button to add new todo
 button.addEventListener("click", addTodo);
@@ -36,18 +22,19 @@ function addTodo() {
     } else {
         input.value = "";
         todo.push({ text: inputValue, done: false });
-        console.log(todo);
         renderTodo();
     }
 }
 
-function renderTodo() {
+function renderTodo(list = todo) {
     let html = "";
 
-    todo.forEach((item) => {
+    list.forEach((item) => {
         const li = `
            <li class="item">
-                <input type="checkbox" id="checkbox" />
+                <input type="checkbox" id="checkbox" ${
+                    item.done ? "checked" : ""
+                }/>
                 <p class="text">${item.text}</p>
                 <button id="removeTodo">
                     <img src="icon/trash.svg" width="24" />
@@ -57,25 +44,48 @@ function renderTodo() {
         html += li;
     });
     ul.innerHTML = html;
-}
 
-// button remove todo
-const btnRemove = document.querySelectorAll("#removeTodo");
-btnRemove.forEach((item, index) => {
-    item.addEventListener("click", () => {
-        todo.splice(index, 1);
-        console.log(todo);
-        renderTodo();
+    // button remove todo
+    const btnRemove = document.querySelectorAll("#removeTodo");
+    btnRemove.forEach((item, index) => {
+        item.addEventListener("click", () => {
+            todo.splice(index, 1);
+            renderTodo();
+        });
     });
-});
+
+    // Checkbox
+    const checkbox = document.querySelectorAll("#checkbox");
+    checkbox.forEach((e, i) => {
+        e.addEventListener("change", () => {
+            todo[i].done = !todo[i].done;
+        });
+    });
+}
 
 const filters = document.querySelector(".filters");
 filters.addEventListener("click", (element) => {
+    document.querySelectorAll(".filters button").forEach((button) => {
+        button.id = "";
+    });
     if (element.target.matches(".all")) {
-        console.log(todo);
+        document.querySelector(".all").id = "on";
+        renderTodo();
     } else if (element.target.matches(".active")) {
+        document.querySelector(".active").id = "on";
         buttonActive();
     } else if (element.target.matches(".completed")) {
+        document.querySelector(".completed").id = "on";
         buttonCompleted();
     }
 });
+
+function buttonActive() {
+    const active = todo.filter((item) => !item.done);
+    renderTodo(active);
+}
+
+function buttonCompleted() {
+    const completed = todo.filter((item) => item.done);
+    renderTodo(completed);
+}
